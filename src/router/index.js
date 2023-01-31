77cba36bd136e772a179d90fbfcb846b4e1802b1
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-01-31 14:49:26 -0300

    feat: integrated the register page

--- src/router/index.js
+++ src/router/index.js
@@ -5,49 +5,42 @@ import AboutPage from '../views/AboutPage.vue'
 import PaymentPage from '../views/PaymentPage.vue'
 import LoginPage from '../views/LoginPage.vue'
 import PostPage from '../views/PostPage.vue'
+import RegisterPage from '../views/RegisterPage.vue'
 
 const routes = [
     {
         path: '/',
         name: 'HomePage',
         component: HomePage
-    },
-    {
+    }, {
         path: '/sobre',
         name: 'AboutPage',
         component: AboutPage
-    },
-    {
+    }, {
         path: '/planos',
         name: 'PaymentPage',
         component: PaymentPage
-    },
-    {
+    }, {
         path: '/login',
         name: 'LoginPage',
         component: LoginPage
-    },
-    {
+    }, {
         path: '/post',
         name: 'PostPage',
         component: PostPage
-    },
-    {
+    }, {
+        path: '/registrar-se',
+        name: 'Register',
+        component: RegisterPage
+    }, {
         path: '',
         name: '',
         component: ''
-    },
-    {
+    }, {
         path: '',
         name: '',
         component: ''
-    },
-    {
-        path: '',
-        name: '',
-        component: ''
-    },
-    {
+    }, {
         path: '',
         name: '',
         component: ''
