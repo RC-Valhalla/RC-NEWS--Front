55ad7121316b2a1e155b45b384a0c68047d197dd
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-01-31 13:34:06 -0300

    feat(migration): home page integrated in the / route

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 </script>
 
 <template>
+  <!--
+    warning: the only propurse of this page is just to render the correct layout
+    based on the current route. Normally, the global componnents (those that are
+    present in each route) would be defined here.
+  -->
   <router-view/>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import store from './store'
 import './styles/base/bootstrap-v7.0.5.css'
 import './styles/globals.scss'
 
+import './styles/home.css'
+
 createApp(App)
   .use(store)
   .use(router)
