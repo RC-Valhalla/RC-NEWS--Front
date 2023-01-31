dca3370d69691f2c9270409fa0da0960e265a169
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-01-31 14:06:22 -0300

    feat: integrated the about page (empty)

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,18 @@
+<script>
+  import TheNavbar from '@/layouts/TheNavbar.vue'
+  import TheFooter from '@/layouts/TheFooter.vue'
+
+  export default {
+    components: {
+      TheNavbar,
+      TheFooter
+    }
+  }
+</script>
+
+
+<template>
+  <TheNavbar/>
+  <!-- empty page... -->
+  <TheFooter/>
+</template>
